e8384334a09eeb67581f8299a0d77cdc92dbbe3f
Omar Hegazy <omarhegazy78@gmail.com>
2023-04-25 16:37:19 -0400

    DebugCommit

---
+++ iOSDevProj/Container.swift
@@ -0,0 +1,15 @@
+//
+//  Container.swift
+//  iOSDevProj
+//
+//  Created by Omar Hegazy on 4/22/23.
+//
+
+import Foundation
+import UIKit
+
+class Container: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
